e43efb9094db88946f4d506fdecb3f2d28353a68
akotek <kotekmath@gmail.com>
2020-04-02 17:41:37 +0300

    bow file

--- src/vicarious/tokenizer.clj
+++ src/vicarious/tokenizer.clj
@@ -1,32 +1,56 @@
 (ns vicarious.tokenizer
   (:require [clojure.string :as s]
-            [clojure.set :as set']))
+            [clojure.set :as set']
+            [clojure.java.io :as io]))
 
-; ==========================================
+; ============================================================
 ;; utils
 
-(defn chars-set [xs]
+(defn unigrams [xs]
   (reduce
     (fn [s w] (set'/union s (set (char-array w)))) #{} xs))
 
-; ==========================================
-;; API
+(defn pair-freqs [m]
+  (reduce #(merge-with + %1 %2)
+          (for [[k v] m]
+            (zipmap (partition 2 1 k) (repeat v)))))
+
+(defn count' [s x]
+  (if (get s x)
+    (update s x inc)
+    (assoc s x 1)))
+
+(defn word-count [words]
+  (->> words
+       (map #(s/lower-case %))
+       (reduce count' {})))
+
+(defn char-count [s]
+  (->> s
+       (char-array)
+       (reduce count' {})))
 
 (defn tokenize [text]
   (as-> text t
         (s/trim t)
-        (filter #(or (Character/isSpace %) (Character/isLetterOrDigit ^Character %)) t)
+        (filter #(or (Character/isSpace %) (Character/isLetterOrDigit ^Character %) (= % \_)) t)
         (apply str t)
         (s/lower-case t)
         (s/split t #"\s+")
-        (distinct t)
+        ;(distinct t)
         (into [] t)))
 
-(defn bow [words]
-  (->> words
-       (map #(s/lower-case %))
-       (reduce (fn [s w] (if (contains? s w)
-                           (update s w inc)
-                           (assoc s w 1))) {})))
+; ============================================================
+;; API
+
+(defn bow-file [file]
+  (with-open [rdr (io/reader file)]
+    (reduce (fn [m l]
+              (as-> l line
+                    (tokenize line)
+                    (word-count line)
+                    (merge-with + m line)))
+            {} (line-seq rdr))))
+
 
 ; ==========================================
--- test/vicarious/tokenizer_test.clj
+++ test/vicarious/tokenizer_test.clj
@@ -10,43 +10,39 @@
 ; ==========================================
 
 (def num-tests 100)
-(def max-tries 50)
 
 (defn un-frequencies [m]
   (mapcat
     (fn [[k v]]
       (repeat v k)) m))
 
-(defn lowercase? [s]
-  (and
-    (not (s/blank? s))
-    (= s (s/lower-case s))))
-
 ; ==========================================
 ;; tests
 
-(def s "The 2 QUICK Brown-foxes jumped over the lazy dog's bone.")
+(def s "The 2 QUICK Brown-foxes jumped over the lazy_ dog's bone.")
 
-(def tokens ["the", "2", "quick", "brownfoxes", "jumped", "over", "lazy", "dogs", "bone"])
+(def tokens ["the", "2", "quick", "brownfoxes", "jumped", "over", "lazy_", "dogs", "bone"])
 
 (deftest test-tokenize
-  (testing "given a string, should return vector of tokens, defined as words (lowercase, w/o whitespaces, punctuation and dup's)"
+  (testing "given a string, should return vector of tokens (defined as words) lower-cased, w/o whitespaces and punctuation,
+  '_' represents end-of-word"
     (is (= (tokenize s) tokens))))
 
-(defspec tokenize-is-alphanumeric num-tests
+(defspec test-tokenize-props num-tests
          (prop/for-all [text gen/string-ascii]
                        (let [res (first (tokenize text))]
                          (and (<= (count res) (count text))
-                              (some? (re-find #"^[a-zA-Z0-9]*$" res))))))
+                              (some? (re-find #"^[a-zA-Z0-9_]*$" res))))))
 
 (deftest test-bow
   (testing "given words, should return a map of lowered-case words with their frequency in the document"
-    (is (= (frequencies tokens) (bow tokens)))))
+    (is (= (word-count tokens) (frequencies tokens)))))
 
-(defspec bow-properties num-tests
-         (prop/for-all [v (gen/such-that (complement empty?) (gen/vector gen/string-alphanumeric))]
-                       (let [lowered (filter #((complement s/blank?) %) (map #(s/lower-case %) v))
-                             res (bow lowered)]
+(defspec test-bow-prop num-tests
+         (prop/for-all [v (gen/such-that (complement empty?) (gen/vector
+                                                               (gen/such-that (complement s/blank?) gen/string-alphanumeric)))]
+                       (let [lowered (map #(s/lower-case %) v)
+                             res (word-count lowered)]
                          (and (<= (count (keys res)) (count lowered))
                               (= (count lowered) (reduce + (vals res)))
                               (= (set lowered) (set (keys res)))
